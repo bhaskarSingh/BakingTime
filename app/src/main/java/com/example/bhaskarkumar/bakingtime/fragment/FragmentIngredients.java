8ed78db57bc05a885c6866ed87c4ecdb22cd2576
Bhaskar kumar <allstarbhaskarkumar@gmail.com>
2018-01-20 20:37:41 +0530

    Reslove issue with ingredient list going blank on orientation change on tablet

--- app/src/main/java/com/example/bhaskarkumar/bakingtime/fragment/FragmentIngredients.java
+++ app/src/main/java/com/example/bhaskarkumar/bakingtime/fragment/FragmentIngredients.java
@@ -20,6 +20,7 @@ import java.util.ArrayList;
 public class FragmentIngredients extends Fragment {
 
     private static final String LOG_TAG = IngredientsList.class.getSimpleName();
+    private static final String INGREDIENT_ARRAYLIST_KEY = "ingredient-arraylist-key";
     private ArrayList<Ingredients> ingredients;
     private RecyclerView recipeRV;
     private IngredientsListAdapter mIngredientsListAdapter;
@@ -29,6 +30,9 @@ public class FragmentIngredients extends Fragment {
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.recycler_view_ingredient_list, container, false);
+        if (savedInstanceState != null){
+            ingredients = savedInstanceState.getParcelableArrayList(INGREDIENT_ARRAYLIST_KEY);
+        }
 
         recipeRV = view.findViewById(R.id.ingredientsListRecyclerView);
         mIngredientsListAdapter = new IngredientsListAdapter(ingredients);
@@ -47,5 +51,11 @@ public class FragmentIngredients extends Fragment {
      */
     public void setIngredients(ArrayList<Ingredients> ingredient){
         ingredients = ingredient;
+    }
+
+    @Override
+    public void onSaveInstanceState(@NonNull Bundle outState) {
+        outState.putParcelableArrayList(INGREDIENT_ARRAYLIST_KEY, ingredients);
+        super.onSaveInstanceState(outState);
     }
 }
